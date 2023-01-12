1691a8e013d73c81b565553de9a60f52b849d2fc
SenaSerefoglu <sena.serefoglu.9@hotmail.com>
2023-01-12 17:24:42 +0300

    Necessary functions for final project

---
+++ final project/functions.py
@@ -0,0 +1,22 @@
+import numpy as np
+def calculate_cdf(histogram):
+    # Get the cumulative sum of the elements
+    cdf = histogram.cumsum()
+
+    # Normalize the cdf
+    normalized_cdf = cdf / float(cdf.max())
+
+    return normalized_cdf
+
+
+def calculate_lookup(src_cdf, ref_cdf):
+    lookup_table = np.zeros(256)
+    lookup_val = 0
+    for src_pixel_val in range(len(src_cdf)):
+        lookup_val
+        for ref_pixel_val in range(len(ref_cdf)):
+            if ref_cdf[ref_pixel_val] >= src_cdf[src_pixel_val]:
+                lookup_val = ref_pixel_val
+                break
+        lookup_table[src_pixel_val] = lookup_val
+    return lookup_table
